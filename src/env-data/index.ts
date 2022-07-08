e4c864222d53980bb71b67b70df25d477fb90059
dtysky <dtysky@outlook.com>
2022-07-08 13:36:06 +0800

    Fix binary env-data

--- src/env-data/index.ts
+++ src/env-data/index.ts
@@ -95,16 +95,36 @@ async function execOne(input: string, output: string, size?: number, bin?: boole
     fs.writeFileSync(path.resolve(output, skyboxPath), skyboxImg);
     fs.writeFileSync(path.resolve(output, 'data.json'), JSON.stringify(json, null, 2), {encoding: 'utf-8'});
   } else {
-    const prefix = Buffer.from('wx-xr-frame-env-data', 'utf-8');
-    json.skybox.map = 0;
-    json.specular.map = skyboxImg.byteLength;
+    const prefix = Buffer.from('wxxr-env', 'utf-8');
+    const skyboxImgFill = align4(skyboxImg);
+    json.skybox.map = {
+      offset: 0, length: skyboxImg.byteLength,
+      type: rgb ? 'image/jpg' : 'image/png'
+    };
+    const specularImgFill = align4(specularImg);
+    json.specular.map = {
+      offset: skyboxImg.byteLength + skyboxImgFill.byteLength, length: specularImg.byteLength,
+      type: !hdr ? 'image/jpg' : 'image/png'
+    };
     const content = Buffer.from(JSON.stringify(json), 'utf-8');
-    const jsonLen = new Uint32Array([content.byteLength])
-    const finalData = Buffer.concat([prefix, new Uint8Array(jsonLen.buffer), content, new Uint8Array(skyboxImg), new Uint8Array(specularImg)]);
+    const jsonLen = new Uint32Array([content.byteLength]);
+    const binFill = align4(content);
+    const binStart = new Uint32Array([content.byteLength + binFill.byteLength]);
+    const finalData = Buffer.concat([
+      prefix, new Uint8Array(jsonLen.buffer), new Uint8Array(binStart.buffer),
+      content, binFill,
+      new Uint8Array(skyboxImg), skyboxImgFill,
+      new Uint8Array(specularImg), specularImgFill
+    ]);
     fs.writeFileSync(path.resolve(output, 'data.bin'), finalData);
   }
 
   showInfo(`输出完成 '${input}' to '${output}/data.${bin ? 'bin' : 'json'}'`);
+}
+
+function align4(buffer: Buffer | ArrayBuffer) {
+  const det = buffer.byteLength % 4;
+  return Buffer.alloc(4 - det);
 }
 
 function getOutput(isFolder: boolean, i: string, o?: string): string {
